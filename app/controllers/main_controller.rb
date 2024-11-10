f09248b70d651006817a2a122d564cb1d997ecef
Boipelo Zuma <pelothedeveloper@gmail.com>
2024-11-10 15:04:59 +0200

    part 10: flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def index
+    ## Example of using flash:
+    # flash[:notice] = "Logged in successfully"
+    # flash.now[:alert] = "Invalid email or password" # By default, flashes persist across pages, but using flash.now ensures it only shows on this page
   end
 end
